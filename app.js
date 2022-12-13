c753a9cc0d225e30f9f2f5fa3a27579656d04476
bearyu99 <ryu.kultra@gmail.com>
2022-12-14 00:23:19 +0900

    mobile section modify

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const menu = document.querySelectorAll("#menu li");
 const img = "https://hankookhos.site/images/lee_portrait.png";
 const ham = document.querySelector(".menu");
+const bg = document.querySelector(".bg-portrait > img");
+const nav = document.querySelector(".nav-btn-group");
+let menuWide = false;
 
 function toggleMenu() {
   ham.classList.toggle("open");
@@ -10,6 +13,11 @@ function toggleMenu() {
     () => document.querySelector("#menu").classList.toggle("fade"),
     100
   );
+  if (menuWide == 0) {
+    menuWide = true;
+  } else {
+    menuWide = false;
+  }
 }
 
 function selectRemove() {
@@ -25,17 +33,42 @@ function sectionVisible(id) {
     e.style.display = "none";
     if (e.id == id + "-section") {
       e.style.display = "";
+      if (e.id == "home-section") {
+        bg.style.visibility = "";
+        bg.src = "/images/portrait.png";
+      } else {
+        bg.style.visibility = "hidden";
+      }
     }
   });
 }
 
-ham.addEventListener("click", (e) => {
+document.body.addEventListener("click", (e) => {
+  const pNode = e.target.parentNode;
+  const div = document.getElementById("mobile-menu");
+  // 메뉴가 열려있는 상태에서
+  if (div.classList.contains("wide")) {
+    let selectNav = e.target.closest("#mobile-nav");
+    // #mobile-nav가 아닌 다른 곳을 클릭하면
+    if (!selectNav) {
+      // 메뉴를 접는다
+      ham.classList.remove("open");
+      document.querySelector(".menu-group").classList.remove("wide");
+      document.querySelector("#menu").classList.remove("visible");
+      document.querySelector("#menu").classList.add("fade");
+      menuWide = false;
+    }
+  }
+});
+
+nav.addEventListener("click", (e) => {
   toggleMenu();
 });
 
 menu.forEach((list) => {
   list.addEventListener("click", (e) => {
     const id = e.target.id;
+    toggleMenu();
     selectRemove();
     switch (id) {
       case "home":
@@ -44,7 +77,6 @@ menu.forEach((list) => {
         e.target.classList.add("select");
         // 섹션 활성화
         sectionVisible(id);
-        document.body.style.backgroundImage = "url(" + img + ")";
         break;
       case "reserve":
         console.log("You clicked " + id);
@@ -52,15 +84,12 @@ menu.forEach((list) => {
         e.target.classList.add("select");
         // 섹션 활성화
         sectionVisible(id);
-        document.body.style.backgroundImage = "none";
-        break;
       case "info":
         console.log("You clicked " + id);
         // Select 클래스 추가
         e.target.classList.add("select");
         // 섹션 활성화
         sectionVisible(id);
-        document.body.style.backgroundImage = "none";
         break;
       case "service":
         console.log("You clicked " + id);
@@ -68,10 +97,25 @@ menu.forEach((list) => {
         e.target.classList.add("select");
         // 섹션 활성화
         sectionVisible(id);
-        document.body.style.backgroundImage = "none";
         break;
       default:
         break;
     }
   });
 });
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
